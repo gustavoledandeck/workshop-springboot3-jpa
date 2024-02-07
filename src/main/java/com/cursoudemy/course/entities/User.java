deee78e8b89330d5dc4585b89b8bf4eae41b49ee
Gustavo Ledandeck <guledandeck2@hotmail.com>
2024-02-07 14:49:36 -0300

    Order, instant, ISO 8601.

--- src/main/java/com/cursoudemy/course/entities/User.java
+++ src/main/java/com/cursoudemy/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.cursoudemy.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -24,6 +27,9 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -73,8 +79,13 @@ public class User implements Serializable{
 		return password;
 	}
 
+	
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
@@ -93,6 +104,8 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 }
